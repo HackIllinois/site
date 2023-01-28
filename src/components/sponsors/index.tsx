ec288198de6061f570b4c70a24d00201bbb33270
Srikar Manikonda <avatsara3@gmail.com>
2023-01-27 23:20:37 -0600

    more fixes

--- src/components/sponsors/index.tsx
+++ src/components/sponsors/index.tsx
@@ -4,11 +4,14 @@ import styles from './styles.module.scss';
 const Section = () => {
     return (
         <div className={styles.sponsors}>
+            <div className={styles.container}>
             <div className={styles.rect}>
                 <h1 className={styles.h1}>SPONSORS</h1>
             </div>
       <div className={styles.emailBox}>
+    <h2 className={styles.email}> Interested in Sponsoring HackIllinois?</h2>
     <h2 className = {styles.email}>Email us at <span className={styles.email}>sponsor@hackillinois.org</span></h2>
+</div>
 </div>
         </div>
     );
