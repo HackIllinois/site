6ccb371206081dbe55fddd72880c8c1218b1fefc
Srikar Manikonda <avatsara3@gmail.com>
2023-02-03 12:05:22 -0600

    please work

--- src/components/sponsors/index.tsx
+++ src/components/sponsors/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 
 import logo1 from 'assets/sponsors/BATS.svg';
 import logo2  from 'assets/sponsors/BlueHill.svg';
-import logo3 from 'assets/sponsors/Epic Logo.svg';
+import logo3 from 'assets/sponsors/Epic logo.svg';
 import logo4 from 'assets/sponsors/Github_Logo.svg';
 import logo5 from 'assets/sponsors/Caterpillar.svg';
 import logo6 from 'assets/sponsors/John-deere.svg';
@@ -22,8 +22,8 @@ const Section = () => {
     <img  className = {styles.logo5} src = {logo2}/>
     <img  className = {styles.logo3} src = {logo3}/>
     <img  className = {styles.logo2} src = {logo4}/>
-    <img  className = {styles.logo1} src = {logo5}/>
-    <img  className = {styles.logo6} src = {logo6}/>
+    <img  className = {styles.logo6} src = {logo5}/>
+    <img  className = {styles.logo1} src = {logo6}/>
     <img className = {styles.logo7} src = {logo7} />
     <img className = {styles.logo8} src = {logo8} />
 
