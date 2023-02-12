90b7fe197bdc1a03e43f6f0becfbef1d175b8c98
Nick King <nrking0@gmail.com>
2023-02-12 15:16:13 -0600

    Add prizes

--- src/pages/prizes/index.tsx
+++ src/pages/prizes/index.tsx
@@ -1,14 +1,17 @@
 import styles from "./styles.module.scss";
+import Navbar from "components/Navbar";
 
 import BOOTH from "../../assets/prizes/booth.svg";
-import HACKLOGO from '../../assets/prizes/hacklogo.svg';
-import GITHUB from '../../assets/prizes/githubprize.svg';
-import BALLOONS from '../../assets/prizes/balloons.svg';
-import BUBBLES from '../../assets/prizes/bubbles.svg';
+import HACKLOGO from "../../assets/prizes/hacklogo.svg";
+import GITHUB from "../../assets/prizes/githubprize.svg";
+import BALLOONS from "../../assets/prizes/balloons.svg";
+import BUBBLES from "../../assets/prizes/bubbles.svg";
+import DEERE from "../../assets/prizes/deere.svg";
 
 const Prizes: React.FC = () => {
     return (
         <div className={styles.prizesMain}>
+            <Navbar path="/prizes" />
             <img src={BUBBLES} className={styles.bubbles} />
             <img src={BOOTH} className={styles.booth} />
             <div className={styles.prizeList}>
@@ -19,7 +22,7 @@ const Prizes: React.FC = () => {
                         <h3>(Staff Pick)</h3>
                         <br />
                         <p>First Place:</p>
-                        <p>$500</p>
+                        <p>$400</p>
                         <p>Second Place</p>
                         <p>$250</p>
                     </div>
@@ -29,19 +32,27 @@ const Prizes: React.FC = () => {
                         <h3>(Sponsored by GitHub)</h3>
                         <br />
                         <p>First Place:</p>
-                        <p>$500</p>
-                        
+                        <p>$400</p>
                     </div>
                     <div className={styles.prize}>
-                    <h3>To Be Announced!</h3>
+                        <img src={HACKLOGO} className={styles.logo} />
+                        <h3>General Track</h3>
+                        <br />
+                        <p>First Place:</p>
+                        <p>$400</p>
                     </div>
                 </div>
                 <div className={styles.prizeColumn}>
                     <div className={styles.prize}>
-                    <h3>To Be Announced!</h3>
+                        <img src={DEERE} className={styles.logo} />
+                        <h3>Community & Sustainability Track</h3>
+                        <h3>(Sponsored by John Deere)</h3>
+                        <br />
+                        <p>First Place:</p>
+                        <p>$400</p>
                     </div>
                     <div className={styles.prize}>
-                    <h3>To Be Announced!</h3>
+                        <h3>To Be Announced!</h3>
                     </div>
                     <div className={styles.prize}>
                         <h3>To Be Announced!</h3>
