41c2c3b4afa089ea2e76d1581aece2a39c61d95a
Aniket Gargya <gargya.aniket@gmail.com>
2023-02-12 23:48:58 -0600

    :speech_balloon: fix travel itenerary

--- src/pages/travel/index.tsx
+++ src/pages/travel/index.tsx
@@ -5,7 +5,6 @@ import clsx from 'clsx';
 import styles from './styles.module.scss';
 import FRONT_LIGHTS from '../../assets/travel/lights.svg';
 import UMBRELLA from '../../assets/travel/umbrella.svg';
-import STAND from '../../assets/travel/stand.svg';
 import STAND_BOTTOM from '../../assets/travel/stand_bottom.svg';
 
 const Travel = () => {
@@ -20,40 +19,44 @@ const Travel = () => {
         <div className={styles.standWrapper}>
           <h1>Travel Details</h1>
           <div className={styles.standContent}>
-            <h2>Purdue To UIUC - 2/24</h2>
+            <h2>Indiana To UIUC - 2/24</h2>
             <table>
                 <tbody>
                     <tr>
                         <td className={styles.leftColumn}>Pickup</td>
                         <td>
-                            <div className={styles.time}>9:00 AM</div>
-                            <div className={styles.location}>Purdue</div>
+                            <div className={styles.time}>11:30 AM EST</div>
+                            <div className={styles.location}>Computer and Information Science Department, 732 W Michigan St, Indianapolis, IN</div>
+                            <div className={styles.time}>1:30 PM EST</div>
+                            <div className={styles.location}>Richard and Patricia Lawson Computer Science Building, 305 N University St, West Lafayette, IN</div>
                         </td>
                     </tr>
                     <tr>
                         <td className={styles.leftColumn}>Arrival</td>
                         <td>
-                            <div className={styles.time}>2:30 PM</div>
+                            <div className={styles.time}>2:30 PM CST</div>
                             <div className={styles.location}>Thomas M. Siebel Center for Computer Science, 201 N. Goodwin Ave, Urbana, IL</div>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <h2>UIUC To Purdue - 2/26</h2>
+            <h2>UIUC To Indiana - 2/26</h2>
             <table>
                 <tbody>
                     <tr>
                         <td className={styles.leftColumn}>Pickup</td>
                         <td>
-                            <div className={styles.time}>9:00 AM</div>
-                            <div className={styles.location}>Purdue</div>
+                            <div className={styles.time}>4:30 PM CST</div>
+                            <div className={styles.location}>Thomas M. Siebel Center for Computer Science, 201 N. Goodwin Ave, Urbana, IL</div>
                         </td>
                     </tr>
                     <tr>
                         <td className={styles.leftColumn}>Arrival</td>
                         <td>
-                            <div className={styles.time}>2:30 PM</div>
-                            <div className={styles.location}>Thomas M. Siebel Center for Computer Science, 201 N. Goodwin Ave, Urbana, IL</div>
+                            <div className={styles.time}>7:30 PM EST</div>
+                            <div className={styles.location}>Richard and Patricia Lawson Computer Science Building, 305 N University St, West Lafayette, IN</div>
+                            <div className={styles.time}>9:30 PM EST</div>
+                            <div className={styles.location}>Richard and Patricia Lawson Computer Science Building, 305 N University St, West Lafayette, IN</div>
                         </td>
                     </tr>
                 </tbody>
