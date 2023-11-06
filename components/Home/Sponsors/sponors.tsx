a725e04c691345456fadf8ea4668298c1f5e674c
Nick King <nrking0@gmail.com>
2023-11-06 15:03:13 -0600

    Almost finish component

--- components/Home/Sponsors/sponors.tsx
+++ components/Home/Sponsors/sponors.tsx
@@ -1,14 +1,19 @@
 "use client"
-
 import React from "react";
 import styles from "./styles.module.scss";
 import Lottie from "lottie-react";
-import crystalBall from "@/public/home/sponsors/crystalball.json"
+import crystalBall from "@/public/home/sponsors/crystalball.json";
+import Image from "next/image";
+import background from "@/public/home/sponsors/background.svg";
 
 const Sponsors: React.FC  =() => {
 
     return (
         <div className={styles.sponsorsMain}>
+            <div className={styles.sponsorsBackground}>
+                <h1>Our Sponsors</h1>
+                <Image className={styles.backgroundSVG} alt="background" src={background} />
+            </div>
             <Lottie className={styles.ball} animationData={crystalBall} loop={true} />
         </div>
     );
