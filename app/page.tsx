a885f5c6de61d0b7956a864a2a433349c5a3cda6
Sarena Yang <sarenay2@illinois.edu>
2023-11-07 00:14:02 -0600

    join us component added, working on cloud divider

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import styles from "./page.module.scss";
 import Hero from "@/components/Home/Hero/hero";
 import Faq from "@/components/Home/Faq/Faq";
 import Events from "@/components/Home/Events/events";
-
+import JoinUs from "@/components/Home/JoinUs/JoinUs";
 
 const Home: React.FC = () => {
     return (
         <main className={styles.main}>
             <Hero/>
             <Events />
+            <JoinUs />
             <Faq/>
         </main>
     );
