51b05bd0924534aabc0948cc12c72b4923435165
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2024-01-15 02:36:59 -0600

    Added Results Failed component

--- app/knights/challenge/page.tsx
+++ app/knights/challenge/page.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import styles from "./page.module.scss";
 import Banner from "@/components/HackKnightChallenge/Banner";
 import KnightChallenge from "@/components/HackKnightChallenge/KnightChallenge";
+import Failed from "@/components/HackKnightChallenge/Failed";
 
 const Challenge = () => {
     const [show, setShow] = React.useState<string>("banner");
     return (
         <div className={styles.background}>
             <div className={styles.container}>
-                {show === "banner" && <KnightChallenge setShow={setShow} />}
+                {show === "challenge" ? (
+                    <KnightChallenge setShow={setShow} />
+                ) : show === "result" ? (
+                    <Failed setShow={setShow} />
+                ) : (
+                    <Banner setShow={setShow} />
+                )}
             </div>
         </div>
     );
