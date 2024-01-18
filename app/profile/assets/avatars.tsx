3a5298e16536d887dff2371707d496d2077b412b
Jish2 <j.j.jappiecraft@gmail.com>
2024-01-17 18:28:12 -0800

    avatar index

---
+++ app/profile/assets/avatars.tsx
@@ -0,0 +1,19 @@
+import BunnyAvatar from "./avatars/bunny.svg";
+import ChipmunkAvatar from "./avatars/chipmunk.svg";
+import YodaAvatar from "./avatars/yoda.svg";
+import CatAvatar from "./avatars/cat.svg";
+import MushroomAvatar from "./avatars/mushroom.svg";
+import FisherAvatar from "./avatars/fisher.svg";
+import GarfieldAvatar from "./avatars/garfield.svg";
+import FishAvatar from "./avatars/fish.svg";
+
+export const avatars = [
+    { name: "bunny", icon: BunnyAvatar },
+    { name: "Chipmunk", icon: ChipmunkAvatar },
+    { name: "Yoda", icon: YodaAvatar },
+    { name: "Cat", icon: CatAvatar },
+    { name: "Mushroom", icon: MushroomAvatar },
+    { name: "Fisher", icon: FisherAvatar },
+    { name: "Garfield", icon: GarfieldAvatar },
+    { name: "Fish", icon: FishAvatar }
+];
