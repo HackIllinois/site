e3d4e480c1617ce63cadab4eada24bdfe152637d
Jish2 <j.j.jappiecraft@gmail.com>
2024-01-17 18:28:07 -0800

    other assets

---
+++ app/profile/assets/close-icon.tsx
@@ -0,0 +1,31 @@
+import { SVGProps } from "react";
+
+type Props = {} & SVGProps<SVGSVGElement>;
+
+const CloseIcon = (props: Props) => {
+    return (
+        <svg
+            width="20"
+            height="19"
+            viewBox="0 0 20 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M18.4141 1L1.41406 18"
+                stroke="#FFB23E"
+                stroke-width="2"
+                stroke-linecap="round"
+            />
+            <path
+                d="M1 1L18 18"
+                stroke="#FFB23E"
+                stroke-width="2"
+                stroke-linecap="round"
+            />
+        </svg>
+    );
+};
+
+export default CloseIcon;
