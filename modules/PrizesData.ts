691d38016938cf716e34412c4f5fa1541139f3de
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2024-02-15 18:54:37 -0600

    Added prizes data

---
+++ modules/PrizesData.ts
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Knights',
+        logo: 'prizes/hack-logo.svg',
+        prize: '$2,000'
+    },
+    {
+        id: 2,
+        name: 'General',
+        logo: 'prizes/hack-logo.svg',
+        prize: '$500'
+    },
+    {
+        id: 3,
+        name: 'Beginner',
+        logo: 'prizes/hack-logo.svg',
+        prize: '$200'
+    },
+    {
+        id: 4,
+        name: 'Best Game Made With Solana Blockchain',
+        logo: 'prizes/solana-logo.svg',
+        prize: '$3,000'
+    },
+    {
+        id: 5,
+        name: 'Hardware Track',
+        logo: 'prizes/john-deere-logo.svg',
+        prize: 'Attendees keep the kits'
+    },
+    {
+        id: 3,
+        name: 'Best Developer Tool',
+        logo: 'prizes/warp-logo.svg',
+        prize: 'Keychron Keyboards to Each Member'
+    },
+]
