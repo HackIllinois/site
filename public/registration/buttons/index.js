f1388c11a437793af4143902d633a52d6cfc91e4
J164 <edleyjacob@gmail.com>
2024-10-09 16:09:17 -0500

    prepare repository for 2025

--- public/registration/buttons/index.js
+++
@@ -1,8 +0,0 @@
-export const EducationButton = require("./education button.svg");
-export const HackspecificButton = require("./hack-specific button.svg");
-export const LoadingButton = require("./loading button.svg");
-export const PersonalButton = require("./personal button.svg");
-export const ReviewButton = require("./review button.svg");
-export const SubmitButton = require("./submit button.svg");
-export const BackButton = require("./back-button.svg");
-export const NextButton = require("./next-button.svg");
