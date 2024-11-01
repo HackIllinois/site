2682355aa0b7b755c21631e47b0f278c4f2a5ffd
Taran Polavarapu <166148260+TaranP1@users.noreply.github.com>
2024-11-01 16:07:16 -0500

    Added new dropdown menu component (#215)
    * Added new dropdown menu component
    
    * Overall changes to dropdown menu component
    
    ---------
    
    Co-authored-by: J164 <edleyjacob@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import styles from "./page.module.scss";
 import Checkboxes from "@/components/Form/Checkboxes/Checkboxes";
 import TestForm from "@/components/Test/TestForm";
+import MainDropdown from "@/components/DropdownBox/MainDropdown";
 
 const Home: React.FC = () => {
+    const options = ["Option 1", "Option 2", "Option 3","Option 3","Option 3","Option 3","Option 3"];
+    const name = "school";
+
     return (
         <main className={styles.main}>
+            <MainDropdown options={options} name={name} />
             <TestForm />
         </main>
     );
