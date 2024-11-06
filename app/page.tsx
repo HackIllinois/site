e289b17c48270e9e79c4957494dc8a9cf0ccd477
J164 <edleyjacob@gmail.com>
2024-11-06 16:07:15 -0600

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,8 @@
 import React from "react";
-import styles from "./page.module.scss";
-import Checkboxes from "@/components/Form/Checkboxes/Checkboxes";
-import TestForm from "@/components/Test/TestForm";
-import MainDropdown from "@/components/Form/DropdownBox/MainDropdown";
 import Registration from "./register/page";
 
 const Home: React.FC = () => {
-    const options = [
-        "Option 1",
-        "Option 2",
-        "Option 3",
-        "Option 3",
-        "Option 3",
-        "Option 3",
-        "Option 3"
-    ];
-    const name = "school";
-
-    return (
-        // <main className={styles.main}>
-        //     <MainDropdown options={options} name={name} />
-        //     <TestForm />
-        // </main>
-        <Registration />
-    );
+    return <Registration />;
 };
 
 export default Home;
