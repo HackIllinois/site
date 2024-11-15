68e9cd0aab2c0cd512e6e862801a32fbc5d59d70
Taran Polavarapu <166148260+TaranP1@users.noreply.github.com>
2024-11-15 14:43:46 -0600

    Dropdown menu rework (#231)
    * Added some features and bug fixes to the dropdown menu
    
    * Added another feature to dropdown menu

--- components/Form/DropdownBox/DropdownItem.tsx
+++ components/Form/DropdownBox/DropdownItem.tsx
@@ -4,11 +4,16 @@ import "./DropdownItem.scss";
 interface DropdownItemProps {
     option: string;
     onClick: (option: string) => void;
+  
 }
 
 const DropdownItem: React.FC<DropdownItemProps> = ({ option, onClick }) => {
     return (
-        <li className="dropdown-option" onClick={() => onClick(option)}>
+        <li
+            className="dropdown-option"
+            onClick={() => onClick(option)}
+       
+        >
             {option}
         </li>
     );
