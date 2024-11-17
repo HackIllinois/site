9e83bb51e3de2b1640844f18ac576bbecc2167e2
miguelaenlle <bot.developer3@gmail.com>
2024-11-17 12:45:46 -0600

    Uncommented handleCheckUserAppliedStatus function

--- app/register/application-status/page.tsx
+++ app/register/application-status/page.tsx
@@ -37,7 +37,7 @@ const ApplicationStatus: React.FC = () => {
     };
 
     useEffect(() => {
-        // handleCheckUserAppliedStatus();
+        handleCheckUserAppliedStatus();
     }, []);
 
     return (
