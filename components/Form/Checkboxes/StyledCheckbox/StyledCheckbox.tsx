cc54c6577d560d9d8e6c6d5163e0f54e5dbde75e
Becky Blake <ryblake2@illinois.edu>
2024-12-31 21:47:34 -0600

    add xs to multiselect checkboxes (#252)
    * add xs to multiselect checkboxes
    
    * add reverse transition and color boxes on hover
    
    * add transition to hover
    
    * transition on unhover

--- components/Form/Checkboxes/StyledCheckbox/StyledCheckbox.tsx
+++ components/Form/Checkboxes/StyledCheckbox/StyledCheckbox.tsx
@@ -23,23 +23,43 @@ const StyledCheckbox: React.FC<PropTypes> = ({
     style,
     ...props
 }) => (
-    <label
-        className={clsx(
-            styles.checkboxLabel,
-            checked && styles.checked,
-            className
-        )}
-        style={style}
-    >
-        <input
-            type="checkbox"
-            className={styles.checkbox}
-            value={value}
-            checked={checked}
-            {...props}
-        />
-        {label}
-    </label>
+    <div className={styles.checkboxWrapper}>
+        <label
+            className={clsx(
+                styles.checkboxLabel,
+                checked && styles.checked,
+                className
+            )}
+            style={style}
+        >
+            <input
+                type="checkbox"
+                className={styles.checkbox}
+                value={value}
+                checked={checked}
+                {...props}
+            />
+            <svg
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+                className={styles.checkboxSvg}
+            >
+                <path
+                    d="M 10 10 L 90 90"
+                    stroke="#000"
+                    strokeDasharray="113"
+                    strokeDashoffset={checked ? "0" : "113"}
+                />
+                <path
+                    d="M 90 10 L 10 90"
+                    stroke="#000"
+                    strokeDasharray="113"
+                    strokeDashoffset={checked ? "0" : "113"}
+                />
+            </svg>
+            {label}
+        </label>
+    </div>
 );
 
 export default StyledCheckbox;
