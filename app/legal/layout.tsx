a7d1b7fa0cd5ce02e35fa2a992c2407beba7402d
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2025-01-01 19:40:59 -0600

    Add code of conduct and complaint procedures

---
+++ app/legal/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { ReactNode } from "react";
+import styles from "./styles.module.scss";
+import { usePathname } from "next/navigation";
+
+const items = [
+    {
+        name: "Code of Conduct",
+        link: "/legal/code-of-conduct"
+    },
+    {
+        name: "Complaint Procedures",
+        link: "/legal/complaint-procedures"
+    }
+];
+
+export default function LegalLayout({ children }: { children?: ReactNode }) {
+    const pathname = usePathname();
+
+    console.log(pathname);
+
+    return (
+        <div className={styles.page}>
+            <nav className={styles.navbar}>
+                <p>Legal</p>
+                {items.map(({ name, link }) => (
+                    <a
+                        key={name}
+                        className={link === pathname ? styles.current : ""}
+                        href={link}
+                    >
+                        {name}
+                    </a>
+                ))}
+            </nav>
+            <main className={styles.main}>{children}</main>
+        </div>
+    );
+}
