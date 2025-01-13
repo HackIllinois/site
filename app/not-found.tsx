895ef4c12e2cadd5af760ee1c607bb433f5ad2e6
J164 <edleyjacob@gmail.com>
2025-01-12 18:50:57 -0600

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import styles from "./Error.module.scss";
+import Link from "next/link";
+
+const NotFound = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.errorBox}>
+                <h2 className={styles.heading}>Page not found</h2>
+                <p className={styles.text}>
+                    Return to the{" "}
+                    <Link href="/" className={styles.link}>
+                        home page
+                    </Link>
+                    ?
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
