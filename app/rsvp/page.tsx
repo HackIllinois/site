42fa613e070bffa0fb506a3fb27434f8d3deaf42
miguelaenlle <bot.developer3@gmail.com>
2025-01-26 12:51:38 -0600

    Added temporary RSVP component testing page

---
+++ app/rsvp/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+
+const RSVPTestingPage: React.FC = () => {
+    return (
+        <div className={styles.screen}>
+            <h1>RSVP Testing Page</h1>
+            <p>This page will be removed shortly.</p>
+        </div>
+    );
+};
+
+export default RSVPTestingPage;
---
+++ components/AvatarSelector/AvatarSelector.tsx
@@ -0,0 +1,7 @@
+export type AvatarSelectorProps = {};
+
+const AvatarSelector: React.FC<AvatarSelectorProps> = props => {
+    return <div>AvatarSelector</div>;
+};
+
+export default AvatarSelector;
