76fadf955fa0c59552883ce29f81758f2af6dfc2
miguelaenlle <bot.developer3@gmail.com>
2025-01-29 00:08:30 -0600

    Implemented the avatar selector component

--- components/AvatarSelector/AvatarSelector.tsx
+++ components/AvatarSelector/AvatarSelector.tsx
@@ -1,7 +1,57 @@
-export type AvatarSelectorProps = {};
+"use client";
+import { avatars } from "@/modules/avatars";
+import { useField } from "formik";
+import Image from "next/image";
+import styles from "./AvatarSelector.module.scss";
+import clsx from "clsx";
 
-const AvatarSelector: React.FC<AvatarSelectorProps> = props => {
-    return <div>AvatarSelector</div>;
+export type AvatarSelectorProps = {
+    name: string;
+    label: string;
+    required?: boolean;
+};
+
+const AvatarSelector: React.FC<AvatarSelectorProps> = ({
+    name,
+    label,
+    required
+}) => {
+    const [field, meta, helpers] = useField(name);
+    const { setValue } = helpers;
+    const { value } = field;
+
+    return (
+        <div className={styles.container}>
+            <label htmlFor={name}>
+                {label}
+                {required && "*"}
+            </label>
+            <div className={styles.avatarSelector}>
+                {avatars.map(avatar => (
+                    <div
+                        style={{
+                            backgroundColor:
+                                avatar.backgroundColor ?? "transparent"
+                        }}
+                        className={clsx(
+                            styles.avatarIcon,
+                            avatar.name === value && styles.selected
+                        )}
+                        key={avatar.name}
+                        onClick={() => {
+                            setValue(avatar.name);
+                        }}
+                    >
+                        <Image
+                            className={styles.avatarIconImage}
+                            src={avatar.icon}
+                            alt={avatar.name}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default AvatarSelector;
--- modules/avatars.ts
+++ modules/avatars.ts
@@ -10,12 +10,20 @@ import FishAvatar from "@/public/profile/avatars/fish.svg";
 import { Avatars } from "@/util/types";
 
 export const avatars = [
-    { name: Avatars.BUNNY, icon: BunnyAvatar },
-    { name: Avatars.SQUIRREL, icon: ChipmunkAvatar },
-    { name: Avatars.GOBLIN, icon: YodaAvatar },
-    { name: Avatars.CAT, icon: CatAvatar },
-    { name: Avatars.MUSHROOM, icon: MushroomAvatar },
-    { name: Avatars.FISHERCAT, icon: FisherAvatar },
-    { name: Avatars.CHESTER, icon: GarfieldAvatar },
-    { name: Avatars.AXOLOTL, icon: FishAvatar }
+    { name: Avatars.BUNNY, icon: BunnyAvatar, backgroundColor: "#f0f0f0" },
+    {
+        name: Avatars.SQUIRREL,
+        icon: ChipmunkAvatar,
+        backgroundColor: "#f0f0f0"
+    },
+    { name: Avatars.GOBLIN, icon: YodaAvatar, backgroundColor: "#f0f0f0" },
+    { name: Avatars.CAT, icon: CatAvatar, backgroundColor: "#f0f0f0" },
+    {
+        name: Avatars.MUSHROOM,
+        icon: MushroomAvatar,
+        backgroundColor: "#f0f0f0"
+    },
+    { name: Avatars.FISHERCAT, icon: FisherAvatar, backgroundColor: "#f0f0f0" },
+    { name: Avatars.CHESTER, icon: GarfieldAvatar, backgroundColor: "#f0f0f0" },
+    { name: Avatars.AXOLOTL, icon: FishAvatar, backgroundColor: "#f0f0f0" }
 ];
