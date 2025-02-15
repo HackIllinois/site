3c6f1f373367a4aea0e4bf765391ce75607d4533
Becky Blake <ryblake2@illinois.edu>
2025-02-15 14:41:27 -0600

    add new background

--- components/Home/Olympian/Olympian.tsx
+++ components/Home/Olympian/Olympian.tsx
@@ -9,6 +9,15 @@ import OlympianButton from "@/components/OlympianButton/OlympianButton";
 import Description from "../Description/Description";
 import GlobalContext from "@/app/context";
 
+// import APHRODITE from "@/public/home/olympian/aprodite.svg";
+// import ARTEMIS from "@/public/home/olympian/artemis.svg";
+// import ATHENA from "@/public/home/olympian/athena.svg";
+// import APOLLO from "@/public/home/olympian/apollo.svg";
+// import HADES from "@/public/home/olympian/hades.svg";
+// import PERSEPHONE from "@/public/home/olympian/persephone.svg";
+// import POSEIDON from "@/public/home/olympian/poseidon.svg";
+// import ZEUS from "@/public/home/olympian/zeus.svg";
+
 const Olympian: React.FC = () => {
     const { eventStatus } = useContext(GlobalContext);
 
