7f35a2c2e9494924ff44d4c0fda6ab4790ed9fd0
Becky Blake <ryblake2@illinois.edu>
2025-02-19 13:39:42 -0600

    Update Landing Page With Characters (#302)
    * add character svgs
    
    * add new background
    
    * add characters
    
    * mysterious leaf
    
    * make background svgs smaller

--- components/Home/Olympian/Olympian.tsx
+++ components/Home/Olympian/Olympian.tsx
@@ -1,16 +1,37 @@
 "use client";
 import styles from "./Olympian.module.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import clsx from "clsx";
 
 import Image from "next/image";
 import LOGO from "@/public/home/olympian/logo.svg";
 import BACKGROUND from "@/public/home/olympian/background.svg";
+import SECOND_LAYER_BACKGROUND from "@/public/home/olympian/second-layer-background.svg";
 import OlympianButton from "@/components/OlympianButton/OlympianButton";
 import Description from "../Description/Description";
 import GlobalContext from "@/app/context";
+import LEAF from "@/public/home/olympian/leaf.svg";
+
+import APHRODITE from "@/public/home/olympian/aphrodite.svg";
+import ARTEMIS from "@/public/home/olympian/artemis.svg";
+import ATHENA from "@/public/home/olympian/athena.svg";
+import APOLLO from "@/public/home/olympian/apollo.svg";
+import HADES from "@/public/home/olympian/hades.svg";
+import PERSEPHONE from "@/public/home/olympian/persephone.svg";
+import POSEIDON from "@/public/home/olympian/poseidon.svg";
+import ZEUS from "@/public/home/olympian/zeus.svg";
 
 const Olympian: React.FC = () => {
     const { eventStatus } = useContext(GlobalContext);
+
+    const [clickCount, setClickCount] = useState(0);
+    const handleClick = () => {
+        setClickCount(clickCount + 1);
+        if (clickCount + 1 === 3) {
+            window.location.href =
+                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Replace with your external link
+        }
+    };
 
     const getButtonData = () => {
         switch (eventStatus) {
@@ -33,10 +54,63 @@ const Olympian: React.FC = () => {
                 />
                 <OlympianButton {...getButtonData()} bottomPadding />
             </div>
+            <div className={styles.characters}>
+                <Image
+                    alt="Apollo"
+                    src={APOLLO}
+                    className={clsx(styles.apollo, styles.character)}
+                />
+                <Image
+                    alt="Athena"
+                    src={ATHENA}
+                    className={clsx(styles.athena, styles.character)}
+                />
+                <Image
+                    alt="Hades"
+                    src={HADES}
+                    className={clsx(styles.hades, styles.character)}
+                />
+                <Image
+                    alt="Persephone"
+                    src={PERSEPHONE}
+                    className={clsx(styles.persephone, styles.character)}
+                />
+                <Image
+                    alt="Zeus"
+                    src={ZEUS}
+                    className={clsx(styles.zeus, styles.character)}
+                />
+                <Image
+                    alt="Poseidon"
+                    src={POSEIDON}
+                    className={clsx(styles.poseidon, styles.character)}
+                />
+                <Image
+                    alt="Artemis"
+                    src={ARTEMIS}
+                    className={clsx(styles.artemis, styles.character)}
+                />
+                <Image
+                    alt="Aphrodite"
+                    src={APHRODITE}
+                    className={clsx(styles.aphrodite, styles.character)}
+                />
+            </div>
             <Image
                 alt="background"
                 src={BACKGROUND}
                 className={styles.background}
+            />
+            <Image
+                alt="second layer background"
+                src={SECOND_LAYER_BACKGROUND}
+                className={styles.secondLayerBackground}
+            />
+            <Image
+                alt="leaf"
+                src={LEAF}
+                className={styles.leaf}
+                onClick={handleClick}
             />
             <Description />
         </section>
