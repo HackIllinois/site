68d90b58d8d7284ef7101f176029784eeb042de1
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2025-02-21 14:44:24 -0600

    Travel Page (#307)
    * Created the prizes page (#301)
    
    * Created the prizes page
    
    * New prizes page added
    
    * Fixes and addition of prizes page
    
    * Fixes to prizes page
    
    ---------
    
    Co-authored-by: Taran Polavarapu <taranpolavarapu@Tarans-MacBook-Pro.local>
    
    * Implemented the travel page
    
    * Reverted changes from taran's prizes page branch
    
    * Added SVGs
    
    * prevent overflow on desktop
    
    ---------
    
    Co-authored-by: Taran Polavarapu <166148260+TaranP1@users.noreply.github.com>
    Co-authored-by: Taran Polavarapu <taranpolavarapu@Tarans-MacBook-Pro.local>
    Co-authored-by: Jacob <edleyjacob@gmail.com>

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -41,6 +41,10 @@ const Navbar = () => {
     const navbarItems: NavbarItem[] = [
         ...getDynamicNavItems(),
         {
+            title: "Travel",
+            link: "/travel"
+        },
+        {
             title: "Schedule",
             link: "/schedule"
         },
