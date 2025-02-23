7723fdfdd7f06a5505498641f4bc089b8b6e4aad
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2025-02-22 18:16:42 -0600

    Prizes Page Responsiveness Improvements (#303)
    * Added prize; added backgrounds; added hack logo
    
    * Further optimization

--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -1,16 +1,39 @@
 "use client";
 
-import styles from "./styles.module.scss";
+import Prize from "@/components/Prize/Prize";
 import Background from "@/public/prizes/backgrounds/prize_background.svg";
-import medal from "@/public/prizes/items/medal.svg";
+
 import Head from "next/head";
+import styles from "./styles.module.scss";
+import HACK_LOGO from "@/public/prizes/items/hack-logo.svg";
+import ATHENA_PRIZE_INFO_CHARACTER from "@/public/prizes/characters/athena-prize-info.svg";
 import Image from "next/image";
 
 const prizesList = [
-    { title: "First Place", amount: "$5000" },
-    { title: "Second Place", amount: "$3000" },
-    { title: "Third Place", amount: "$1500" },
-    { title: "Fourth Place", amount: "$500" }
+    {
+        title: "First Place",
+        subheader: "(General)",
+        amount: 5000,
+        imageSrc: HACK_LOGO.src
+    },
+    {
+        title: "Second Place",
+        subheader: "(General)",
+        amount: 3000,
+        imageSrc: HACK_LOGO.src
+    },
+    {
+        title: "Third Place",
+        subheader: "(General)",
+        amount: 1500,
+        imageSrc: HACK_LOGO.src
+    },
+    {
+        title: "Fourth Place",
+        subheader: "(General)",
+        amount: 500,
+        imageSrc: HACK_LOGO.src
+    }
 ];
 
 const Prizes: React.FC = () => {
@@ -30,7 +53,6 @@ const Prizes: React.FC = () => {
                     minHeight: "100vh",
                     width: "100%",
                     display: "flex",
-                    flexDirection: "column",
                     alignItems: "flex-start",
                     justifyContent: "flex-start",
                     overflowY: "auto",
@@ -38,25 +60,43 @@ const Prizes: React.FC = () => {
                 }}
                 className={styles.container}
             >
+                <div className={styles.leftDiv}>
+                    <h1 className={styles.title}>Prizes</h1>
+
+                    <Image
+                        className={styles.character}
+                        src={ATHENA_PRIZE_INFO_CHARACTER.src}
+                        width={400}
+                        height={700}
+                        alt="Athena"
+                    />
+                </div>
                 <div className={styles.scrollableGrid}>
                     <div className={styles.prizesGrid}>
                         {prizesList.map((prize, index) => (
-                            <div key={index} className={styles.medalContainer}>
-                                <Image
-                                    src={medal.src}
-                                    alt="Medal"
-                                    width={300}
-                                    height={300}
-                                    className={styles.medalIcon}
-                                />
+                            <Prize
+                                key={index}
+                                header={prize.title}
+                                subheader={prize.subheader}
+                                imageSrc={prize.imageSrc}
+                                amount={prize.amount}
+                            />
+                            // <div key={index} className={styles.medalContainer}>
+                            //     <Image
+                            //         src={medal.src}
+                            //         alt="Medal"
+                            //         width={300}
+                            //         height={300}
+                            //         className={styles.medalIcon}
+                            //     />
 
-                                <div className={styles.medalOverlay1}>
-                                    <h1>{prize.title}</h1>
-                                </div>
-                                <div className={styles.medalOverlay2}>
-                                    <h2>{prize.amount}</h2>
-                                </div>
-                            </div>
+                            //     <div className={styles.medalOverlay1}>
+                            //         <h1>{prize.title}</h1>
+                            //     </div>
+                            //     <div className={styles.medalOverlay2}>
+                            //         <h2>{prize.amount}</h2>
+                            //     </div>
+                            // </div>
                         ))}
                     </div>
                 </div>
