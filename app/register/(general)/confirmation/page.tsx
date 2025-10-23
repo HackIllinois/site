a4d6ddb042b7903d3bfac2b4b870551f2cadb616
gracetzeng <zengt.grace@gmail.com>
2025-10-22 20:23:02 -0500

    Initial commit

--- app/register/(general)/confirmation/page.tsx
+++ app/register/(general)/confirmation/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import styles from "./styles.module.scss";
-import OlympianButton from "@/components/OlympianButton/OlympianButton";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import {
     isAuthenticated,
@@ -34,55 +33,72 @@ const Confirmation: React.FC = () => {
     }, []);
 
     return (
-        <>
-            {isLoading && <Loading />}
-            <div className={styles.container}>
-                <div className={styles.contentWrapper}>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}>Application Submitted!</h1>
-                        <p className={styles.message}>
-                            Thank you for signing up for HackIllinois 2025!
-                            Please check your email for your registration
-                            confirmation.
-                        </p>
-                        <p className={styles.follow}>
-                            Be sure to follow our Instagram (
-                            <a
-                                href="https://www.instagram.com/hackillinois"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={styles.link}
-                            >
-                                @hackillinois
-                            </a>
-                            ) and X (
-                            <a
-                                href="https://twitter.com/hackillinois"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={styles.link}
-                            >
-                                @hackillinois
-                            </a>
-                            ) for additional information!
-                        </p>
-                    </div>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center"
+            }}
+        >
+            <Box // APPLICATION FINISHED
+                sx={{
+                    display: "inline-flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}
+            >
+                <Typography
+                    variant="h3"
+                    sx={{
+                        fontWeight: 700,
+                        color: "white",
+                        lineHeight: 1,
+                        mb: 0.5,
+                        textAlign: "center"
+                    }}
+                >
+                    APPLICATION
+                </Typography>
+                <Typography
+                    variant="h2"
+                    sx={{
+                        fontWeight: 800,
+                        color: "grey.200",
+                        lineHeight: 1,
+                        mb: 3,
+                        width: "100%"
+                    }}
+                >
+                    FINISHED
+                </Typography>
+            </Box>
 
-                    <OlympianButton
-                        text="Profile"
-                        link="/profile"
-                    ></OlympianButton>
-                </div>
-            </div>
-            <div className={styles.zeus}>
-                <Image
-                    src="/registration/characters/zeus.svg"
-                    alt="zeus"
-                    fill
-                    style={{ objectFit: "contain" }}
-                />
-            </div>
-        </>
+            <Typography
+                variant="h6"
+                sx={{
+                    color: "white",
+                    fontWeight: 400,
+                    mb: 1,
+                    fontSize: { xs: "1rem", md: "1.25rem" }
+                }}
+            >
+                Thank you for signing up for HackIllinois 2026!
+            </Typography>
+
+            <Typography
+                variant="h6"
+                sx={{
+                    color: "white",
+                    fontWeight: 400,
+                    mb: 5,
+                    fontSize: { xs: "1rem", md: "1.25rem" }
+                }}
+            >
+                Please check the status of your account in your email.
+            </Typography>
+        </Box>
     );
 };
 
