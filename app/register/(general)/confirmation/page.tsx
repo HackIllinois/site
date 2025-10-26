d755f3d42743463ec9c40de3e7ec4af4dbb5fa70
gracetzeng <zengt.grace@gmail.com>
2025-10-26 02:38:49 -0500

    implement hifi designs with responsiveness

--- app/register/(general)/confirmation/page.tsx
+++ app/register/(general)/confirmation/page.tsx
@@ -9,6 +9,18 @@ import {
 } from "@/util/api";
 import { usePathname, useRouter } from "next/navigation";
 import Loading from "@/components/Loading/Loading";
+import { Montserrat, Tsukimi_Rounded } from "next/font/google";
+
+// TODO: move into theme file
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    weight: ["400", "600", "700"]
+});
+
+const tsukimi = Tsukimi_Rounded({
+    subsets: ["latin"],
+    weight: ["500", "600", "700"]
+});
 
 const Confirmation: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -39,9 +51,32 @@ const Confirmation: React.FC = () => {
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
-                textAlign: "center"
+                textAlign: "center",
+                mt: "40px",
+                mb: "150px"
             }}
         >
+            <Box
+                sx={{
+                    position: "absolute",
+                    justifyContent: "center",
+                    top: "80px",
+                    transform: { md: "rotate(5.93deg)" },
+                    width: { xs: "374px", md: "748px" },
+                    height: { xs: "305px", md: "610px" },
+                    zIndex: 0, // behind the text
+                    opacity: 0.34,
+                    pointerEvents: "none"
+                }}
+            >
+                <Image
+                    src="/registration/26.svg"
+                    alt="The number 26 in background."
+                    fill
+                    style={{ objectFit: "contain" }}
+                />
+            </Box>
+
             <Box // APPLICATION FINISHED
                 sx={{
                     display: "inline-flex",
@@ -52,7 +87,9 @@ const Confirmation: React.FC = () => {
                 <Typography
                     variant="h3"
                     sx={{
+                        fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
                         fontWeight: 700,
+                        fontSize: { xs: "40px", md: "80px" },
                         color: "white",
                         lineHeight: 1,
                         mb: 0.5,
@@ -64,8 +101,9 @@ const Confirmation: React.FC = () => {
                 <Typography
                     variant="h2"
                     sx={{
+                        fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
                         fontWeight: 800,
-                        color: "grey.200",
+                        fontSize: { xs: "60px", md: "120px" },
                         lineHeight: 1,
                         mb: 3,
                         width: "100%"
@@ -78,10 +116,10 @@ const Confirmation: React.FC = () => {
             <Typography
                 variant="h6"
                 sx={{
-                    color: "white",
-                    fontWeight: 400,
-                    mb: 1,
-                    fontSize: { xs: "1rem", md: "1.25rem" }
+                    fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
+                    fontWeight: 600,
+                    fontSize: { xs: "11px", md: "22px" },
+                    color: "white"
                 }}
             >
                 Thank you for signing up for HackIllinois 2026!
@@ -90,14 +128,30 @@ const Confirmation: React.FC = () => {
             <Typography
                 variant="h6"
                 sx={{
+                    fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
+                    fontWeight: 600,
+                    fontSize: { xs: "11px", md: "22px" },
                     color: "white",
-                    fontWeight: 400,
-                    mb: 5,
-                    fontSize: { xs: "1rem", md: "1.25rem" }
+                    mb: "17px"
                 }}
             >
                 Please check the status of your account in your email.
             </Typography>
+
+            <Box
+                sx={{
+                    position: "relative",
+                    width: { xs: "136px", md: "189px" },
+                    height: { xs: "42px", md: "59px" }
+                }}
+            >
+                <Image
+                    src="/registration/logo.svg"
+                    alt="White HackIllinois logo"
+                    fill
+                    style={{ objectFit: "contain" }}
+                />
+            </Box>
         </Box>
     );
 };
