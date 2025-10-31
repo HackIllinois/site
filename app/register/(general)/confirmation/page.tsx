869e82fb8042e0ef48eedafca01930eb344a0730
gracetzeng <zengt.grace@gmail.com>
2025-10-30 22:19:58 -0500

    increase 26 image opacity, adjust text size on smaller screens

--- app/register/(general)/confirmation/page.tsx
+++ app/register/(general)/confirmation/page.tsx
@@ -53,103 +53,118 @@ const Confirmation: React.FC = () => {
                 justifyContent: "center",
                 textAlign: "center",
                 mt: "40px",
-                mb: "150px"
+                mb: { xs: "60px", md: "150px" }
             }}
         >
             <Box
                 sx={{
-                    position: "absolute",
-                    justifyContent: "center",
-                    top: "80px",
-                    width: { xs: "374px", md: "748px" },
-                    height: { xs: "305px", md: "610px" },
-                    zIndex: 0, // behind the text
-                    opacity: 0.34,
-                    pointerEvents: "none"
-                }}
-            >
-                <Image
-                    src="/registration/26.svg"
-                    alt="The number 26 in background."
-                    fill
-                    style={{ objectFit: "contain" }}
-                />
-            </Box>
-
-            <Box // APPLICATION FINISHED
-                sx={{
-                    display: "inline-flex",
-                    flexDirection: "column",
-                    alignItems: "center"
-                }}
-            >
-                <Typography
-                    variant="h3"
-                    sx={{
-                        fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
-                        fontWeight: 700,
-                        fontSize: { xs: "40px", md: "80px" },
-                        color: "white",
-                        lineHeight: 1,
-                        mb: 0.5,
-                        textAlign: "center"
-                    }}
-                >
-                    APPLICATION
-                </Typography>
-                <Typography
-                    variant="h2"
-                    sx={{
-                        fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
-                        fontWeight: 800,
-                        fontSize: { xs: "60px", md: "120px" },
-                        lineHeight: 1,
-                        mb: 3,
-                        width: "100%"
-                    }}
-                >
-                    FINISHED
-                </Typography>
-            </Box>
-
-            <Typography
-                variant="h6"
-                sx={{
-                    fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
-                    fontWeight: 600,
-                    fontSize: { xs: "11px", md: "22px" },
-                    color: "white"
-                }}
-            >
-                Thank you for signing up for HackIllinois 2026!
-            </Typography>
-
-            <Typography
-                variant="h6"
-                sx={{
-                    fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
-                    fontWeight: 600,
-                    fontSize: { xs: "11px", md: "22px" },
-                    color: "white",
-                    mb: "17px"
-                }}
-            >
-                Please check the status of your account in your email.
-            </Typography>
-
-            <Box
-                sx={{
                     position: "relative",
-                    width: { xs: "136px", md: "189px" },
-                    height: { xs: "42px", md: "59px" }
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    maxWidth: "800px"
                 }}
             >
-                <Image
-                    src="/registration/logo.svg"
-                    alt="White HackIllinois logo"
-                    fill
-                    style={{ objectFit: "contain" }}
-                />
+                <Box
+                    sx={{
+                        position: "absolute",
+                        justifyContent: "center",
+                        top: 0,
+                        width: { xs: "90vw", md: "748px" },
+                        height: { xs: "70vw", md: "610px" },
+                        zIndex: 0, // behind the text
+                        pointerEvents: "none"
+                    }}
+                >
+                    <Image
+                        src="/registration/26.svg"
+                        alt="The number 26 in background."
+                        fill
+                        style={{
+                            objectFit: "contain",
+                            objectPosition: "top center"
+                        }}
+                    />
+                </Box>
+
+                <Box
+                    sx={{
+                        zIndex: 1,
+                        display: "inline-flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}
+                >
+                    <Typography
+                        variant="h3"
+                        sx={{
+                            fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
+                            fontWeight: 700,
+                            fontSize: { xs: "40px", sm: "60px", md: "80px" },
+                            color: "white",
+                            lineHeight: 1,
+                            mb: 0.5
+                        }}
+                    >
+                        APPLICATION
+                    </Typography>
+
+                    <Typography
+                        variant="h2"
+                        sx={{
+                            fontFamily: `${tsukimi.style.fontFamily}, sans-serif`,
+                            fontWeight: 800,
+                            fontSize: { xs: "60px", sm: "100px", md: "120px" },
+                            lineHeight: 1,
+                            mb: 3,
+                            width: "100%"
+                        }}
+                    >
+                        FINISHED
+                    </Typography>
+
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
+                            fontWeight: 600,
+                            fontSize: { xs: "11px", sm: "22px" },
+                            color: "white"
+                        }}
+                    >
+                        Thank you for signing up for HackIllinois 2026!
+                    </Typography>
+
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            fontFamily: `${montserrat.style.fontFamily}, sans-serif`,
+                            fontWeight: 600,
+                            fontSize: { xs: "11px", sm: "22px" },
+                            color: "white",
+                            mb: "17px"
+                        }}
+                    >
+                        Please check the status of your account in your email.
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            position: "relative",
+                            width: { xs: "136px", sm: "189px" },
+                            height: { xs: "42px", sm: "59px" }
+                        }}
+                    >
+                        <Image
+                            src="/registration/logo.svg"
+                            alt="White HackIllinois logo"
+                            fill
+                            style={{ objectFit: "contain" }}
+                        />
+                    </Box>
+                </Box>
             </Box>
         </Box>
     );
