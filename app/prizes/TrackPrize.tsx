fa6d50eea37cea185cd22f0f0b856b450fd75d14
Shreepaad <shreepaad.earanti@gmail.com>
2025-12-31 20:50:13 -0600

    made the prizes page
    made the prizes page

---
+++ app/prizes/TrackPrize.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import React, { useId } from "react";
+import { Box } from "@mui/material";
+
+type TrackPrizeProps = {
+    backgroundSrc: string;
+
+    topText: string;
+    bottomText: string;
+    topTextOffset: number;
+    bottomTextOffset: number;
+
+    bottomTextSize: number;
+    topTextSize?: number;
+
+    width: number;
+    height: number;
+
+    centerOffsetY?: number;
+    centerOffsetX?: number;
+    bottomLetterSpacing?: number;
+
+    radiusX: number;
+    radiusY: number;
+};
+
+const TrackPrize: React.FC<TrackPrizeProps> = ({
+    backgroundSrc,
+    topText,
+    bottomText,
+    topTextOffset,
+    bottomTextOffset,
+    bottomTextSize,
+    width,
+    height,
+    centerOffsetX = 0,
+    centerOffsetY = 0,
+    topTextSize = 20,
+    bottomLetterSpacing = 0,
+    radiusX,
+    radiusY
+}) => {
+    const uid = useId();
+    const topArcId = `topArc-${uid}`;
+    const bottomArcId = `bottomArc-${uid}`;
+
+    const scaleFinal = {
+        xs: 0.6,
+        sm: 0.53,
+        md: 0.76,
+        lg: 1,
+        xl: 1
+    };
+
+    const size = {
+        xs: Math.round(height * scaleFinal.xs),
+        sm: Math.round(height * scaleFinal.sm),
+        md: Math.round(height * scaleFinal.md),
+        lg: Math.round(height * scaleFinal.lg),
+        xl: Math.round(height * scaleFinal.xl)
+    };
+
+    return (
+        <Box
+            sx={{
+                width: size,
+                height: size,
+                backgroundImage: `url("${backgroundSrc}")`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                position: "relative"
+            }}
+        >
+            <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 400 400"
+                style={{
+                    position: "absolute",
+                    inset: 0,
+                    marginTop: `${centerOffsetY}px`,
+                    marginLeft: `${centerOffsetX}px`
+                }}
+            >
+                <defs>
+                    <path
+                        id={topArcId}
+                        d={`
+                    M 200,200
+                    m -${radiusX},0
+                    a ${radiusX},${radiusY} 0 0 1 ${radiusX * 2},0
+                `}
+                    />
+
+                    <path
+                        id={bottomArcId}
+                        d={`
+                M 200,200
+                m -${radiusX},0
+                a ${radiusX},${radiusY} 0 0 0 ${radiusX * 2},0
+            `}
+                    />
+                </defs>
+
+                {/* top text */}
+                <text
+                    fill="#fff"
+                    fontFamily="Tsukimi Rounded"
+                    fontSize={topTextSize}
+                    fontWeight="700"
+                    textAnchor="middle"
+                >
+                    <textPath
+                        href={`#${topArcId}`}
+                        startOffset={`${topTextOffset}%`}
+                    >
+                        {topText}
+                    </textPath>
+                </text>
+                {/* bottom text */}
+                <text
+                    fill="#fff"
+                    fontFamily="Montserrat"
+                    fontSize={bottomTextSize}
+                    fontWeight="600"
+                    textAnchor="middle"
+                    style={{ letterSpacing: bottomLetterSpacing }}
+                >
+                    <textPath
+                        href={`#${bottomArcId}`}
+                        startOffset={`${bottomTextOffset}%`}
+                    >
+                        {bottomText}
+                    </textPath>
+                </text>
+            </svg>
+        </Box>
+    );
+};
+
+export default TrackPrize;
--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -1,21 +1,399 @@
-import React from "react";
-import styles from "./styles.module.scss";
+"use client";
+import { Box, Typography } from "@mui/material";
+import PathPrize from "./PathPrize";
+import TrackPrize from "./TrackPrize";
 
-const Home: React.FC = () => {
+const Prizes: React.FC = () => {
     return (
-        <main className={styles.main}>
-            <div className={styles.top}>
-                <div className={styles.mainText}>
-                    <div className={styles.title}>
-                        <h1 data-text={"Prizes"}>Prizes</h1>
-                    </div>
-                    <div className={styles.subtitle}>
-                        <span className={styles.text}>coming soon</span>
-                    </div>
-                </div>
-            </div>
-        </main>
+        <Box
+            sx={{
+                width: "100%",
+                minHeight: "100vh",
+                bgcolor: "#020316"
+            }}
+        >
+            <Box
+                sx={{
+                    width: "100%",
+                    minHeight: "100vh",
+                    backgroundImage: {
+                        xs: 'url("/prizes/backgrounds/prizesbg.svg")'
+                    },
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "top center",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    pt: "200px",
+                    pb: "200px"
+                }}
+            >
+                <Typography
+                    sx={{
+                        color: "#FFF",
+                        textAlign: "center",
+                        textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                        fontFamily: "Tsukimi Rounded",
+                        fontSize: "48px",
+                        fontWeight: 700
+                    }}
+                >
+                    PRIZES
+                </Typography>
+
+                <Typography
+                    sx={{
+                        color: "#FFFFFF",
+                        textAlign: "center",
+                        fontFamily: "Montserrat",
+                        fontSize: "20px",
+                        fontWeight: 600,
+                        mt: "10px",
+                        maxWidth: "90%"
+                    }}
+                >
+                    Earn merch, swag, and prize money in the thousands!
+                </Typography>
+
+                <Typography
+                    sx={{
+                        textAlign: "center",
+                        fontFamily: "Montserrat",
+                        fontSize: "30px",
+                        fontStyle: "normal",
+                        fontWeight: 700,
+                        background:
+                            "linear-gradient(270deg, #A315D6 -19.46%, #FDAB60 47.1%, #A315D6 109.92%)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        backgroundClip: "text",
+                        pt: "60px"
+                    }}
+                >
+                    PATH PRIZES
+                </Typography>
+
+                <Typography
+                    sx={{
+                        color: "#FFFFFF",
+                        textAlign: "center",
+                        fontFamily: "Montserrat",
+                        fontSize: "20px",
+                        fontWeight: 400,
+                        maxWidth: "90%"
+                    }}
+                >
+                    Prizes are for each member except Best Voyagers Hack & Best
+                    General Hack.
+                </Typography>
+
+                <Box
+                    sx={{
+                        display: "grid",
+                        gridTemplateColumns: { xs: "90vw", sm: "45vw 45vw" },
+                        gap: "20px",
+                        width: "90vw",
+                        mt: "60px",
+                        justifyItems: { xs: "center", sm: "center" }
+                    }}
+                >
+                    <PathPrize
+                        backgroundSrc="/prizes/path_prizes/prize1.svg"
+                        topText="BEST VOYAGERS HACK"
+                        bottomText="$5000"
+                        radius={100}
+                        width={600}
+                        height={600}
+                        centerOffsetY={10}
+                        bottomTextSize={40}
+                        topTextOffset={40}
+                        bottomTextOffset={65}
+                        bottomLetterSpacing={4}
+                        topGradientWord="VOYAGERS"
+                        topGradient={{
+                            from: "#A315D6",
+                            mid: "#FDAB60",
+                            to: "#A315D6"
+                        }}
+                    />
+
+                    <Box sx={{ mt: { xs: "-100px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize2.svg"
+                            topText="BEST GENERAL HACK"
+                            bottomText="$2500"
+                            radius={110}
+                            width={580}
+                            height={580}
+                            centerOffsetY={10}
+                            centerOffsetX={-10}
+                            bottomTextSize={40}
+                            topTextOffset={60}
+                            bottomTextOffset={40}
+                            bottomLetterSpacing={4}
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "-25px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize3.svg"
+                            topText="BEST BEGINNER HACK"
+                            bottomText="EPOMAKER Mechanical Keyboard"
+                            radius={165}
+                            width={410}
+                            height={410}
+                            centerOffsetY={8}
+                            centerOffsetX={0}
+                            bottomTextSize={30}
+                            topTextOffset={50}
+                            bottomTextOffset={50}
+                            topTextSize={30}
+                            showHelpIcon
+                            helpAngleDeg={338}
+                            helpSize={30}
+                            helpRotationDeg={19}
+                            helpTooltip="At least half of the members are first time hackers."
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "25px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize4.svg"
+                            topText="BEST UI/UX DESIGN"
+                            bottomText="FUJIFILM Camera Package"
+                            radius={160}
+                            width={430}
+                            height={430}
+                            centerOffsetY={12}
+                            centerOffsetX={0}
+                            bottomTextSize={30}
+                            topTextOffset={50}
+                            bottomTextOffset={50}
+                            topTextSize={30}
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "-15px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize5.svg"
+                            topText="BEST SOCIAL IMPACT"
+                            secondText="MARSHALL Speaker +"
+                            bottomText="$50 Donation to charity of choice"
+                            radius={120}
+                            width={580}
+                            height={580}
+                            centerOffsetY={10}
+                            centerOffsetX={10}
+                            bottomTextSize={20}
+                            topTextOffset={40}
+                            bottomTextOffset={56}
+                            topTextSize={20}
+                            showHelpIcon
+                            helpAngleDeg={313}
+                            helpSize={21}
+                            helpRotationDeg={5}
+                            helpTooltip="Recognizes the project that has the potential to create the most significant positive change or address a pressing societal issue. Whether through addressing environmental concerns, improving accessibility, or tackling social injustices, among many other possibilities, this category highlights projects that aim to make a tangible difference in the world."
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "-55px", sm: "10px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize6.svg"
+                            topText="MOST POPULAR"
+                            bottomText="SONY Headphones"
+                            radius={125}
+                            width={590}
+                            height={590}
+                            centerOffsetY={8}
+                            centerOffsetX={-6}
+                            bottomTextSize={20}
+                            topTextOffset={58}
+                            bottomTextOffset={40}
+                            topTextSize={20}
+                            showHelpIcon
+                            helpAngleDeg={330}
+                            helpSize={20}
+                            helpRotationDeg={15}
+                            helpTooltip="Determined by attendee votes."
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "-15px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize7.svg"
+                            topText="MOST CREATIVE"
+                            bottomText="NINJA Coffee Machine"
+                            radius={165}
+                            width={410}
+                            height={410}
+                            centerOffsetY={10}
+                            centerOffsetX={0}
+                            bottomTextSize={30}
+                            topTextOffset={45}
+                            bottomTextOffset={50}
+                            topTextSize={30}
+                        />
+                    </Box>
+
+                    <Box sx={{ mt: { xs: "30px", sm: "0px" } }}>
+                        <PathPrize
+                            backgroundSrc="/prizes/path_prizes/prize8.svg"
+                            topText="MOST USELESS"
+                            bottomText="NERF gun + Walkie-Talkie"
+                            radius={150}
+                            width={470}
+                            height={470}
+                            centerOffsetY={-2}
+                            centerOffsetX={9}
+                            bottomTextSize={25}
+                            topTextOffset={60}
+                            bottomTextOffset={50}
+                            topTextSize={25}
+                            showHelpIcon
+                            helpTooltip="Celebrates projects that are delightfully impractical — but still well-designed and fully functional. We’re not looking for broken demos or unfinished apps, but thoughtfully built projects that explore fun, novel, or whimsical ideas rather than serious real-world problems."
+                            helpAngleDeg={333}
+                            helpSize={28}
+                            helpRotationDeg={15}
+                        />
+                    </Box>
+                </Box>
+
+                <Typography
+                    sx={{
+                        textAlign: "center",
+                        fontFamily: "Montserrat",
+                        fontSize: "30px",
+                        fontStyle: "normal",
+                        fontWeight: 700,
+                        background:
+                            "linear-gradient(270deg, #A315D6 -19.46%, #FDAB60 47.1%, #A315D6 109.92%)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        backgroundClip: "text",
+                        pt: "60px"
+                    }}
+                >
+                    TRACK PRIZES
+                </Typography>
+
+                <Typography
+                    sx={{
+                        color: "#FFFFFF",
+                        textAlign: "center",
+                        fontFamily: "Montserrat",
+                        fontSize: "20px",
+                        fontWeight: 400,
+                        maxWidth: "90%"
+                    }}
+                >
+                    Designed by our sponsors to provide a specialized topic to
+                    center your project around.
+                </Typography>
+
+                <Box
+                    sx={{
+                        display: "grid",
+                        gridTemplateColumns: { xs: "85vw", sm: "1fr 1fr" },
+                        gap: "20px",
+                        width: "90%",
+                        mt: "80px",
+                        justifyItems: "center"
+                    }}
+                >
+                    <TrackPrize
+                        backgroundSrc="/prizes/track_prizes/track1.svg"
+                        topText="BEST USE OF CLOUDFLARE"
+                        bottomText="5k Cloudflare Credits"
+                        radiusX={200}
+                        radiusY={150}
+                        width={500}
+                        height={500}
+                        centerOffsetY={10}
+                        centerOffsetX={13}
+                        bottomTextSize={25}
+                        topTextOffset={50}
+                        bottomTextOffset={50}
+                        topTextSize={25}
+                    />
+
+                    <TrackPrize
+                        backgroundSrc="/prizes/track_prizes/track2.svg"
+                        topText="BEST USE OF OPENAI"
+                        bottomText="5k OpenAI Credits"
+                        radiusX={200}
+                        radiusY={150}
+                        width={500}
+                        height={500}
+                        centerOffsetY={10}
+                        centerOffsetX={13}
+                        bottomTextSize={25}
+                        topTextOffset={50}
+                        bottomTextOffset={50}
+                        topTextSize={25}
+                    />
+
+                    <TrackPrize
+                        backgroundSrc="/prizes/track_prizes/track3.svg"
+                        topText="BEST HARDWARE HACK"
+                        bottomText="Elegoo Hardware Starter Kit"
+                        radiusX={200}
+                        radiusY={150}
+                        width={500}
+                        height={500}
+                        centerOffsetY={10}
+                        centerOffsetX={13}
+                        bottomTextSize={25}
+                        topTextOffset={50}
+                        bottomTextOffset={50}
+                        topTextSize={25}
+                    />
+
+                    <TrackPrize
+                        backgroundSrc="/prizes/track_prizes/track4.svg"
+                        topText="BEST USE OF SUPERMEMORY"
+                        bottomText="[PRIZE]"
+                        radiusX={200}
+                        radiusY={150}
+                        width={500}
+                        height={500}
+                        centerOffsetY={10}
+                        centerOffsetX={13}
+                        bottomTextSize={25}
+                        topTextOffset={50}
+                        bottomTextOffset={50}
+                        topTextSize={25}
+                    />
+                </Box>
+
+                <Box
+                    sx={{
+                        display: { xs: "grid", sm: "" },
+                        gridTemplateColumns: { xs: "85vw", sm: "90vw" },
+                        justifyItems: "center",
+                        width: "90%",
+                        mt: "20px"
+                    }}
+                >
+                    <TrackPrize
+                        backgroundSrc="/prizes/track_prizes/track5.svg"
+                        topText="[TRACK NAME]"
+                        bottomText="[PRIZE]"
+                        radiusX={200}
+                        radiusY={150}
+                        width={500}
+                        height={500}
+                        centerOffsetY={10}
+                        centerOffsetX={13}
+                        bottomTextSize={25}
+                        topTextOffset={50}
+                        bottomTextOffset={50}
+                        topTextSize={25}
+                    />
+                </Box>
+            </Box>
+        </Box>
     );
 };
 
-export default Home;
+export default Prizes;
