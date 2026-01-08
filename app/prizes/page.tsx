38c892eb404dfbdb45f641714209b784295f2ed0
Shreepaad <shreepaad.earanti@gmail.com>
2026-01-08 14:17:11 -0600

    updated banner and content
    updated banner and content

--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -104,6 +104,87 @@ const Prizes: React.FC = () => {
                     initial="hidden"
                     animate="visible"
                     viewport={{ once: true, amount: 0.3 }}
+                >
+                    <motion.div variants={itemVariants}>
+                        <Box
+                            sx={{
+                                backgroundImage: `url("/prizes/rocket.svg")`,
+                                backgroundRepeat: "no-repeat",
+                                backgroundSize: {
+                                    xs: "auto 150px",
+                                    md: "auto 140px"
+                                },
+                                backgroundPosition: {
+                                    xs: "right center",
+                                    md: "center"
+                                },
+                                mr: { xs: 2, md: 0 },
+                                pt: 8,
+                                pb: 9,
+                                width: "100vw"
+                            }}
+                        >
+                            <motion.div variants={itemVariants}>
+                                <Typography
+                                    sx={{
+                                        textAlign: "center"
+                                    }}
+                                >
+                                    <Box
+                                        component="span"
+                                        sx={{
+                                            background:
+                                                "linear-gradient(270deg, #A315D6 -19.46%, #FDAB60 47.1%, #A315D6 109.92%)",
+                                            WebkitBackgroundClip: "text",
+                                            backgroundClip: "text",
+                                            WebkitTextFillColor: "transparent",
+                                            color: "transparent",
+                                            display: "inline-block",
+                                            fontSize: {
+                                                xs: "19px",
+                                                sm: "25px"
+                                            },
+                                            fontWeight: 700,
+                                            maxWidth: "55vw",
+                                            ml: { xs: -15, md: 0 }
+                                        }}
+                                    >
+                                        <Box
+                                            component="span"
+                                            sx={{
+                                                display: {
+                                                    xs: "none",
+                                                    sm: "inline"
+                                                }
+                                            }}
+                                        >
+                                            This year{" "}
+                                        </Box>
+                                        HackIllinois has{" "}
+                                        <Box
+                                            component="span"
+                                            sx={{
+                                                fontSize: {
+                                                    xs: "20px",
+                                                    sm: "35px"
+                                                }
+                                            }}
+                                        >
+                                            $30K+
+                                        </Box>{" "}
+                                        in prizes!
+                                    </Box>
+                                </Typography>
+                            </motion.div>
+                        </Box>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                    viewport={{ once: true, amount: 0.3 }}
                     style={{ width: "90%" }}
                 >
                     <motion.div variants={itemVariants}>
@@ -113,7 +194,7 @@ const Prizes: React.FC = () => {
                                 fontFamily: "Montserrat",
                                 fontSize: "35px",
                                 fontWeight: 700,
-                                pt: "60px"
+                                pt: "20px"
                             }}
                         >
                             <Box
@@ -473,25 +554,14 @@ const Prizes: React.FC = () => {
                                 textAlign: "center",
                                 fontFamily: "Montserrat",
                                 fontSize: "20px",
-                                fontWeight: 600
+                                fontWeight: 600,
+                                width: "70%",
+                                mx: "auto"
                             }}
                         >
                             Designed by our sponsors to provide a specialized
-                            topic to center your project around.
-                        </Typography>
-                    </motion.div>
-
-                    <motion.div variants={itemVariants}>
-                        <Typography
-                            sx={{
-                                color: "#FFFFFF",
-                                textAlign: "center",
-                                fontFamily: "Montserrat",
-                                fontSize: "20px",
-                                fontWeight: 600
-                            }}
-                        >
-                            Each team can only compete in{" "}
+                            topic to center your project around. Each team can
+                            only compete in{" "}
                             <span
                                 style={{
                                     background:
@@ -504,6 +574,31 @@ const Prizes: React.FC = () => {
                                 ONE
                             </span>{" "}
                             track.
+                        </Typography>
+                    </motion.div>
+                    <motion.div variants={itemVariants}>
+                        <Typography
+                            sx={{
+                                color: "#FFFFFF",
+                                textAlign: "center",
+                                fontFamily: "Montserrat",
+                                fontSize: "20px",
+                                fontWeight: 600
+                            }}
+                        >
+                            Each track winning team will also receive a{" "}
+                            <span
+                                style={{
+                                    background:
+                                        "linear-gradient(180deg, #A315D6 -19.46%, #FDAB60 47.1%, #A315D6 109.92%)",
+                                    WebkitBackgroundClip: "text",
+                                    WebkitTextFillColor: "transparent",
+                                    fontWeight: 700
+                                }}
+                            >
+                                $2500
+                            </span>{" "}
+                            cash prize.
                         </Typography>
                     </motion.div>
                 </motion.div>
