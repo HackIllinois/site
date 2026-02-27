8a494b254e04ef6f95b3a977ad45e2bf27f9c62f
miguelaenlle <bot.developer3@gmail.com>
2026-02-27 17:06:22 -0600

    Link directly to stream

--- app/landing/Hero.tsx
+++ app/landing/Hero.tsx
@@ -259,7 +259,7 @@ const Hero = () => {
                         >
                             <GradientButton
                                 text="OPENING CEREMONY LINK"
-                                link="https://www.youtube.com/@hackillinois/streams"
+                                link="https://www.youtube.com/watch?v=MVUfuKi2Oao"
                                 external
                             />
                             <GradientButton
